00060ca33d4f795ce1a7a0257a9bdc47b85077e7
gleiMoura <gle.bar.mou@gmail.com>
2024-10-23 20:03:29 -0300

    🔥 delete old code

--- index.js
+++ index.js
@@ -18,22 +18,6 @@ const createCards = async () => {
         console.log("deu um erro cabuloso, o que será que foi?", error);
         console.error(error);
     }
-
-    axios.get("https://jsonplaceholder.typicode.com/posts").then((response) => {
-        const info = response?.data;
-
-        for (const card of info) {
-            caixas.innerHTML += `
-                <div class="caixa">
-                    <h2>${card.title}</h2>
-                    <p>${card.body}</p>
-                </div>
-            `
-        }
-    }).catch((error) => {
-        console.log("deu um erro cabuloso, o que será que foi?", error);
-        console.error(error);
-    })
 };
 
 createCards();
